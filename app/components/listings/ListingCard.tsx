0ee359e55baab7041f2bcb331ae91a1a5357ace9
yash <yashraj.se10@gmail.com>
2024-01-30 23:02:10 +0530

    Cards section done

---
+++ app/components/listings/ListingCard.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import React, { useCallback, useMemo } from "react";
+import { Listing, Reservition } from "@prisma/client";
+import { SafeUser } from "@/app/types";
+import { useRouter } from "next/navigation";
+import useCountries from "@/app/hooks/useCountries";
+import { format } from "date-fns";
+import Image from "next/image";
+import Button from "../Button";
+
+interface ListingCardProps {
+  data: Listing;
+  reservation?: Reservition;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionId?: string;
+  actionLabel?: string;
+  currentUser?: SafeUser | null;
+}
+
+export default function ListingCard({
+  data,
+  reservation,
+  onAction,
+  disabled,
+  actionLabel,
+  actionId = "",
+  currentUser,
+}: ListingCardProps) {
+  const router = useRouter();
+  const { getByValue } = useCountries();
+
+  const location = getByValue(data.locationValue);
+
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation;
+      if (disabled) {
+        return;
+      }
+      onAction?.(actionId);
+    },
+    [onAction, actionId, disabled]
+  );
+
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
+
+    return data.price;
+  }, [reservation, data.price]);
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, "PP")}- ${format(end, "PP")}`;
+  }, [reservation]);
+
+  return (
+    <div
+      onClick={() => router.push(`/listings/${data.id}`)}
+      className="col-span-1 cursor-pointer group "
+    >
+      <div className=" flex flex-col gap-2 w-full ">
+        <div className=" aspect-square w-full relative overflow-hidden rounded-xl ">
+          <Image
+            fill
+            alt="Listings"
+            src={data.imageSrc}
+            className="
+        object-cover
+        h-full
+        w-full
+        group-hover:scale-110 
+        transition"
+          />
+        </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">month</div>}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || null}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            small
+            disabled={disabled}
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return <EmptyState showReset />;
@@ -10,8 +13,14 @@ export default async function Home() {
 
   return (
     <div className=" px-9 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-      {listings.map((listings: any) => {
-        return <div>{listings.title}</div>;
+      {listings.map((listing: any) => {
+        return (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        );
       })}
     </div>
   );
